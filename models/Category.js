664afa212cd17e3c5fb31648e9aadb24b6e484e4
tbsdvl <tristonburns97@gmail.com>
2021-04-02 00:03:40 -0700

    Updating sequelize model columns

--- models/Category.js
+++ models/Category.js
@@ -4,9 +4,38 @@ const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
+// Category
+
+// id
+
+// Integer.
+
+// Doesn't allow null values.
+
+// Set as primary key.
+
+// Uses auto increment.
+
+// category_name
+
+// String.
+
+// Doesn't allow null values.
+
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
   },
   {
     sequelize,
@@ -14,7 +43,6 @@ Category.init(
     freezeTableName: true,
     underscored: true,
     modelName: 'category',
-  }
-);
+});
 
 module.exports = Category;
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -2,11 +2,54 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
+// ProductTag
+
+// id
+
+// Integer.
+
+// Doesn't allow null values.
+
+// Set as primary key.
+
+// Uses auto increment.
+
+// product_id
+
+// Integer.
+
+// References the Product model's id.
+
+// tag_id
+
+// Integer.
+
+// References the Tag model's id.
+
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
